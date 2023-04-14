9cb74d10eeac1673786fa0d92685bfc60c7237e2
Felipe <carreirofelipe@gmail.com>
2023-04-14 11:10:27 -0300

    contact 2

--- forms/contact.php
+++ forms/contact.php
@@ -20,8 +20,8 @@ $contact->subject = $_POST['subject'];
 // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
 $contact->smtp = array(
     'host' => 'smtp.gmail.com',
-    'username' => '.,
-    'password' => '.',
+    'username' => 'carreirofelipesend@gmail',
+    'password' => 'sendtemp2023',
     'port' => '587',
     'encryption' => 'tls'
 );
